b14ca4d02220ad1ae2ae9375cdcfd73df0f45f3d
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-06-25 14:13:22 +0100

    added the cookie package and replace the regenerate and save function due to the latest package not having it

--- src/app.js
+++ src/app.js
@@ -5,18 +5,39 @@ const helmet = require('helmet');
 const express = require('express');
 const passport = require('passport');
 const compression = require('compression');
+const cookieSession = require('cookie-session');
 const authRouter = require('./routes/auth.route');
-const { AUTH_OPTIONS } = require('./utils/auth.options');
 const secretRouter = require('./routes/secret.route');
 const { Strategy } = require('passport-google-oauth20');
-const { verifyCallback } = require('./utils/verify.callback');
 const isLoggedIn = require('./middlewares/check.login');
+const { AUTH_OPTIONS } = require('./utils/auth.options');
+const { verifyCallback } = require('./utils/verify.callback');
 
 const app = express();
 
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY_1, process.env.COOKIE_KEY_2],
+  })
+);
+app.use((req, res, next) => {
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = cb => {
+      cb();
+    };
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = cb => {
+      cb();
+    };
+  }
+  next();
+});
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 app.use(passport.initialize());
