1cabd7f89aca22dce837060a5da9872f7777670c
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-06-25 08:37:49 +0100

    created the html file and the routes to display the default message

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const compression = require('compression');
+const authRouter = require('./routes/auth.route');
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
+app.use(compression());
+
+app.use('/api/v1/user/auth', authRouter);
+
+app.use('/secret', (req, res) => {
+  return res.send(`Your personal secret is ${92}`);
+});
+
+app.get('/api/v1', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.all('*', (req, res) => {
   res.status(404).send(`The route ${req.originalUrl} does not exist! 💨`, 404);
